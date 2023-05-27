46f39202b58ef979abc180d3c7d49e6c84910483
Sumrit Grover <sumritgrover1@gmail.com>
2023-05-27 19:29:32 +0530

    basic printing and mut variable logic

---
+++ main.rs
@@ -0,0 +1,12 @@
+fn main (){
+    let mut message = "Hello world";
+    println!("The print text is-{}",message);
+    message = "second print";
+    println!("The print text is-{}",message);
+
+    let age = 21;
+    println!("{}", age);
+    let age = 20;
+    println!("{}", age);
+
+}
